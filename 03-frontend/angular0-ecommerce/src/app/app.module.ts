8d303c5d852b3a4cc8f0878db31e7d02c4e36530
theujjwal <66766108+theujjwal@users.noreply.github.com>
2023-07-13 18:58:49 +0530

    idk man

--- 03-frontend/angular0-ecommerce/src/app/app.module.ts
+++ 03-frontend/angular0-ecommerce/src/app/app.module.ts
@@ -5,12 +5,22 @@ import { AppComponent } from './app.component';
 import { ProductListComponent } from './components/product-list/product-list.component';
 import {HttpClientModule } from '@angular/common/http';
 import { ProductService } from './services/product.service';
+import { Routes,RouterModule } from '@angular/router';
+const routes : Routes = [
+  {path:'category/:id',component: ProductListComponent},
+  {path:'categoty',component: ProductListComponent},
+  {path: 'products',component: ProductListComponent},
+  {path: '',redirectTo:'/prodcuts',pathMatch:'full'},
+  {path: '**',redirectTo: '/prodcuts',pathMatch:'full'},
+]
 @NgModule({
   declarations: [
     AppComponent,
     ProductListComponent
-  ],
+  ],  
   imports: [
+    RouterModule.forRoot(routes),
+
     BrowserModule,
     HttpClientModule,
   ],
